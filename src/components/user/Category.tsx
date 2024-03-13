6a146fb8d6aee4200c86f66cddb757d5da0a68b1
Tomoka Sato <tomokasato@MacBookPro.local>
2024-03-13 23:41:36 +0900

    カテゴリーボタンのスタイル切り替え（仮）

--- src/components/user/Category.tsx
+++ src/components/user/Category.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Category = () => {
   const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     const getCategory = async () => {
@@ -17,16 +18,40 @@ const Category = () => {
     getCategory();
   }, []);
 
+  // カテゴリーがクリックされたときの処理
+  const handleCategoryClick = (index: number | null) => {
+    setSelectedCategory(index);
+  };
+
+  // カテゴリーのスタイルを取得する
+  const getCategoryStyle = (index: number | null) => {
+    if (selectedCategory === null && index === null) {
+      return "bg-[#1C1C1C] text-white";
+    } else if (selectedCategory === index) {
+      return "bg-[#1C1C1C] text-white";
+    } else {
+      return "border-2 border-solid border-[#1c1c1c]";
+    }
+  };
+
   return (
     <div className="my-[100px]">
       <ul className="flex">
-        <li className="text-xl mr-6 rounded-full py-3 bg-[#1C1C1C] text-white w-[11rem] flex cursor-pointer">
+        <li
+          className={`${getCategoryStyle(
+            null
+          )} text-xl mr-6 rounded-full py-3 w-[11rem] flex cursor-pointer`}
+          onClick={() => handleCategoryClick(null)}
+        >
           <h3 className="my-0 mx-auto">すべて</h3>
         </li>
         {category.map((item, index) => (
           <li
             key={index}
-            className="text-xl mr-6 rounded-full py-3 border-2 border-solid border-[#1c1c1c] w-[11rem] flex cursor-pointer"
+            className={`${getCategoryStyle(
+              index
+            )} text-xl mr-6 rounded-full py-3 w-[11rem] flex cursor-pointer`}
+            onClick={() => handleCategoryClick(index)}
           >
             <h3 className="my-0 mx-auto">{item.name}</h3>
           </li>
